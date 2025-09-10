6224e679c0964ad3eb55e6e6a456bf7d0a10e395
Akzhol Nuketayev <nuketayev@Akzhols-MacBook-Pro.local>
2025-09-10 21:11:44 +0200

    variables

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("myH1").textContent = `hello`;
+document.getElementById("myP").textContent = `brother`;
+let x=120;
+let price=10.4344;
+console.log(price);
+console.log(`You are worth of ${price}$`)
+console.log(typeof price)
+let email = "akzhol.ca@ca.ca"
