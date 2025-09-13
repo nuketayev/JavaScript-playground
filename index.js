935d9dc53ba4591e8baa776cb21538c08196ad84
Akzhol Nuketayev <nuketayev@Akzhols-MacBook-Pro.local>
2025-09-13 22:33:30 +0200

    counting app

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const PI = 3.14159;
-let radius;
-let circumference;
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
 
-radius = window.prompt('Enter the radius of a circle');
-radius = Number(radius);
-
-circumference = 2 * radius * pi;
-document.getElementById('H1').textContent = `The circumference for the radious of ${radius}cm is: ${circumference}`;
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = `${count}`
+};
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = `${count}`
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = `${count}`
+}
